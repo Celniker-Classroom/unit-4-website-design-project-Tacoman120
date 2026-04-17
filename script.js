f21b8c8ab05560581bed3fa2c85de5e176b142d8
Tacoman120 <90591421+Tacoman120@users.noreply.github.com>
2026-04-17 16:55:17 +0000

    how to play

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
 // add javascript here
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
-let slideIndex = 1;
+let slideIndex = 0;
 showSlides(slideIndex);
 
 // Next/previous controls
